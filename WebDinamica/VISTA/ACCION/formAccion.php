2a7cd9ea1609cf4a3ff5e6814d8ee40b55f6df9a
DuboscqDylan <dylan.duboscq@est.fi.uncoma.edu.ar>
2025-09-03 14:57:01 -0300

    Add files via upload

--- WebDinamica/VISTA/ACCION/formAccion.php
+++ WebDinamica/VISTA/ACCION/formAccion.php
@@ -1,14 +1,5 @@
 <?php
 include_once(__DIR__ . "../../ESTRUCTURA/header.php");
-require_once(__DIR__ . "../../../CONTROL/TP1/EJ1/controlNumero.php");
-require_once(__DIR__ . "../../../CONTROL/TP1/EJ2/controlCursada.php");
-require_once(__DIR__ . "../../../CONTROL/TP1/EJ3/controlInfoPersonal.php");
-require_once(__DIR__ . "../../../CONTROL/TP1/EJ7/controlOperaciones.php");
-require_once(__DIR__ . "../../../CONTROL/TP1/EJ8/controlCine.php");
-require_once(__DIR__ . "../../../CONTROL/TP2/EJ3/controlLogin.php");
-require_once(__DIR__ . "../../../CONTROL/TP2/EJ4/controlCinema.php");
-require_once(__DIR__ . "../../../CONTROL/TP3/EJ1/controlSubirArchivo.php");
-require_once(__DIR__ . "../../../CONTROL/TP3/EJ2/controlMostrarArchivo.php");
 
 
 if ($_POST || $_GET) {
@@ -18,6 +9,7 @@ if ($_POST || $_GET) {
     switch ($accion) {
 
         case 'tp1e1':
+            require_once(__DIR__ . "../../../CONTROL/TP1/EJ1/controlNumero.php");
             $control = new controlNumero();
             $numero = $_GET['numero'];
             $resultado = $control->verificarNumero($numero);
@@ -26,6 +18,7 @@ if ($_POST || $_GET) {
             break;
 
         case 'tp1e2':
+            require_once(__DIR__ . "../../../CONTROL/TP1/EJ2/controlCursada.php");
             $control = new controlCursada();
             $cronograma = $_GET;
             $resultado = $control->calcularCargaHoraria($cronograma);
@@ -34,6 +27,7 @@ if ($_POST || $_GET) {
             break;
 
         case 'tp1e3':
+            require_once(__DIR__ . "../../../CONTROL/TP1/EJ3/controlInfoPersonal.php");
             $control = new controlInfoPersonal();
             $resultado = $control->mostrarInfoPersonal($_GET);
             echo "<p>" . $resultado . "</p>";
@@ -41,6 +35,7 @@ if ($_POST || $_GET) {
             break;
 
         case 'tp1e7':
+            require_once(__DIR__ . "../../../CONTROL/TP1/EJ7/controlOperaciones.php");
             $control = new controlOperaciones();
             $resultado = $control->operar($_GET);
             echo "<p>" . $resultado . "</p>";
@@ -48,6 +43,7 @@ if ($_POST || $_GET) {
             break;
 
         case 'tp1e8':
+            require_once(__DIR__ . "../../../CONTROL/TP1/EJ8/controlCine.php");
             $control = new controlCine();
             $resultado = $control->calcularPrecioEntrada($_GET);
             echo "<p>El precio de la entrada es: " . $resultado . "</p>";
@@ -55,6 +51,7 @@ if ($_POST || $_GET) {
             break;
 
         case 'tp2e3':
+            require_once(__DIR__ . "../../../CONTROL/TP2/EJ3/controlLogin.php");
             $control = new controlLogin();
             $resultado = $control->validarUsuario(["username" => $_POST["username"], "password" => $_POST["password"]]);
             echo "<p>" . $resultado . "</p>";
@@ -62,16 +59,18 @@ if ($_POST || $_GET) {
             break;
 
         case 'tp2e4':
+            require_once(__DIR__ . "../../../CONTROL/TP2/EJ4/controlCinema.php");
             $control = new controlCinema();
             $resultado = $control->validarImagen();
             $pelicula = $_POST;
             $pelicula['archivo'] = $resultado;
             include_once("../../VISTA/ESTRUCTURA/header.php");
-            include_once ("../../VISTA/TP2/EJ4/vistaMostrarPelicula.php");
+            include_once("../../VISTA/TP2/EJ4/vistaMostrarPelicula.php");
             include_once("../../VISTA/ESTRUCTURA/footer.php");
             break;
 
         case 'tp3e1':
+            require_once(__DIR__ . "../../../CONTROL/TP3/EJ1/controlSubirArchivo.php");
             $control = new controlSubirArchivo;
             $resultado = $control->validarSubida();
             echo $resultado;
@@ -79,13 +78,13 @@ if ($_POST || $_GET) {
             break;
 
         case 'tp3e2':
+            require_once(__DIR__ . "../../../CONTROL/TP3/EJ2/controlMostrarArchivo.php");
             $control = new controlMostrarArchivo();
             $resultado = $control->validarSubida();
             echo $resultado;
             echo "<br> <a href=" . "/WebDinamica/VISTA/TP3/EJ2/vistaMostrarArchivo.php class='btn btn-primary mt-3'>Volver a la pagina anterior</a>";
             break;
     }
-
 }
 
 include_once(__DIR__ . "../../ESTRUCTURA/footer.php");
